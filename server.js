6514b4d03b4ba281a3d6e6f228e376202b16d96f
LouisVallaey <louis.vallaey.w0522@student.hogent.be>
2018-08-17 11:26:33 +0200

    2dezit  api permission in server.js --> accessing apis outside app

--- server.js
+++ server.js
@@ -43,6 +43,21 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
+
+
+app.use(expressJwt({
+  secret: config.secret,
+  getToken: function (req) {
+      if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+          return req.headers.authorization.split(' ')[1];
+      } else if (req.query && req.query.token) {
+          return req.query.token;
+      }
+      return null;
+  }
+}).unless({ path: ['/users/authenticate', '/users/register'] })); 
+
+//api
 app.use('/user', user);
 app.use('/package', package);
 
